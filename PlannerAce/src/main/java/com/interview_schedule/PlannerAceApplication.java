88f2f18d2c28b209be09522d3a6150a5d6a8816b
AayushViswase <aayushviswase10@gmail.com>
2023-04-27 22:01:22 +0530

    Project Started

---
+++ PlannerAce/src/main/java/com/interview_schedule/PlannerAceApplication.java
@@ -0,0 +1,13 @@
+package com.interview_schedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlannerAceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlannerAceApplication.class, args);
+	}
+
+}
---
+++ PlannerAce/src/test/java/com/interview_schedule/PlannerAceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interview_schedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerAceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
